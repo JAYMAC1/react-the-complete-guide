a842f4d5e9fa91e3fd57a4efa22814d3540a7dbe
James McNeil <mcneil_james@hotmail.com>
2021-01-19 23:28:59 +0000

    array map function

--- next-gen-js/app.js
+++ next-gen-js/app.js
@@ -5,7 +5,7 @@ const newNumber = number
 
 console.log(newNumber)
 
-// Reference - pointer to info held in memory
+// Reference - pointer to info held in memory (Objects & Arrays)
 const person = {
   name: 'James',
 }
@@ -17,3 +17,10 @@ const secondPerson = {
 person.name = 'Millie'
 
 console.log(secondPerson)
+
+// Array functions
+
+const numbers = [1, 2, 3]
+const doubleNumArray = numbers.map((num) => num * 2)
+console.log(numbers)
+console.log(doubleNumArray)
