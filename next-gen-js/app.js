5df3ff495c6c4442ef554bf2a3404943d0f65607
James McNeil <mcneil_james@hotmail.com>
2021-01-19 23:19:46 +0000

    added playground test

---
+++ next-gen-js/app.js
@@ -0,0 +1,14 @@
+// Primitive - makes copy of variable
+
+const number = 10
+const newNumber = number
+
+console.log(newNumber)
+
+// Reference - pointer to info held in memory
+const person = {
+  name: 'James',
+}
+const secondPerson = person
+
+console.log(secondPerson)
