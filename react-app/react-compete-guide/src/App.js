298aa6449511f7b950eece906590768b4bf53e8a
James McNeil <mcneil_james@hotmail.com>
2021-01-22 14:50:03 +0000

    testing props and state

--- react-app/react-compete-guide/src/App.js
+++ react-app/react-compete-guide/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 48 },
+      { name: 'Manu', age: 47 },
+      { name: 'Stephanie', age: 46 },
+    ],
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!!!')
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>Hi, I'm a react App</h1>
+        <p>This is really working!!!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     )
   }
