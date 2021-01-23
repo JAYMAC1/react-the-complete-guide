f8f78871f5f800d579b8180f968a11972eb12d58
James McNeil <mcneil_james@hotmail.com>
2021-01-23 17:44:20 +0000

    created useroutput component

---
+++ react-app/challeng-one/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useroutput = (props) => {
+  retrurn(
+    <div className='useroutput'>
+      <p>Welcome NameHere</p>
+      <p>This is my attempt at the React Challeneg one</p>
+    </div>
+  )
+}
+
+export default useroutput
