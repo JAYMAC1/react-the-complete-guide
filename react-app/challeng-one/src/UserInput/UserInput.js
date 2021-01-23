2bdd8652c60654005e8a0783d66265410cbf359e
James McNeil <mcneil_james@hotmail.com>
2021-01-23 17:23:56 +0000

    added userinput jsx

--- react-app/challeng-one/src/UserInput/UserInput.js
+++ react-app/challeng-one/src/UserInput/UserInput.js
@@ -1,1 +1,12 @@
 import react from 'react'
+
+const userinput = () => {
+  return (
+    <div className='UserInput'>
+      <p>Please enter username:</p>
+      <input type='text' />
+    </div>
+  )
+}
+
+export default userinput
